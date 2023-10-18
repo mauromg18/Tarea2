ea5928d17c7f15eb2195d3763e5c09ecce4165af
Mauro <mauro.smg.1801@gmail.com>
2023-10-18 19:16:17 -0300

    actualizando

--- src/main/java/Tarea2/Bebida.java
+++ src/main/java/Tarea2/Bebida.java
@@ -1,4 +1,9 @@
 package Tarea2;
 
-public class Bebida {
+public abstract class Bebida extends Producto {
+    public Bebida(int serie) {
+        super(serie);
+    }
+
+    public abstract String beber();
 }
